a36c31f8bc76927f8b60765389e09368f945bd63
Cullen REDDY <cull.redd@gmail.com>
2021-12-17 14:02:02 -0800

    URLs heroku'd

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -74,7 +74,7 @@ const HomePage = () => {
         e.preventDefault();
         const exampleSentences = [collocation[1], collocation[2]];
         console.log(exampleSentences);
-        axios.post(`https://git.heroku.com/habla-me.git/users/collocation`, {
+        axios.post(`https://git.heroku.com/habla-me.git/users/collocation`,  {
             word: palabra,
             phrase: collocation[0],
             example: exampleSentences,
